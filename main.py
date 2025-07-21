5bc41414a89a36ce2626afa47128aa55353284ec
mirkotorrisi <mirko.torrisi92@gmail.com>
2025-07-21 20:42:02 +0200

    feat: Add requirements.txt for project dependencies

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routes import wine_prediction
+import os
 
-
-app = FastAPI(debug=True, title="Wine prediction model API", description="API for managing wine predictions", version="1.0.0")
+app = FastAPI(debug=os.getenv("DEBUG", "false").lower() == "true", title="Wine prediction model API", description="API for managing wine predictions", version="1.0.0")
 
 app.include_router(wine_prediction.router, prefix="/wine", tags=["Wine"])
